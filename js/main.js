54e7d554bc5dea39bf22b0ba9c28eaf0775f689a
Martin <martinsebastian1314@hotmail.com>
2023-08-17 15:47:05 -0300

    Funciones

--- js/main.js
+++ js/main.js
@@ -1,11 +1,76 @@
 
 
 
+var version;
 
-var urlCampeones = "http://ddragon.leagueoflegends.com/cdn/13.1.1/data/en_US/champion.json";
+
+/*Guarda la version actualizada en una variable.*/
+$.ajax({
+    url: "https://ddragon.leagueoflegends.com/api/versions.json",
+    async: false,
+    dataType: 'json',
+    success: function(data) {
+      version = data[0];
+    },
+    error: function(jqxhr, textStatus, error) {
+      console.error('Hubo un error:', textStatus, error);
+    }
+  });
+
+
+var urlCampeones = "http://ddragon.leagueoflegends.com/cdn/"+ version +"/data/en_US/champion.json";
 var campeonesJSON;
 
 var divContenedor = document.getElementById("contenedor");
+
+
+
+
+var campeonesSeleccionados = 0;
+
+
+
+
+function agregarAlArreglo( idElemento, idContenedor ){
+
+
+    var elemento = document.getElementById(idElemento);
+    var contenedor = document.getElementById(idContenedor);
+    var estaEnElArreglo = false;
+
+    console.log(idElemento);
+    //Revisamos que no este repetido
+    for(i=0; i< contenedor.children.length ; i++){
+        if( (contenedor.children[i].id) == elemento.id + "copy" ){
+            estaEnElArreglo = true;
+        }
+    }
+
+
+    //Se agrega al contenedor
+    if (campeonesSeleccionados < 5 && !estaEnElArreglo) {
+        
+        var contenidoDeElemento = elemento.innerHTML;
+
+        var elementoCopia = document.createElement("div");
+        elementoCopia.innerHTML = contenidoDeElemento;
+        elementoCopia.className = "col-2 p-0 m-auto";
+        elementoCopia.id = elemento.id + "copy"
+        elementoCopia.addEventListener('click', function(){elementoCopia.remove(); campeonesSeleccionados--;});
+        
+        contenedor.append(elementoCopia);
+        campeonesSeleccionados++;
+
+    }else if(estaEnElArreglo){
+
+        document.getElementById(idElemento + "copy").remove();
+        campeonesSeleccionados--;
+
+
+    }
+
+}
+
 
 
 
@@ -24,9 +89,11 @@ $.getJSON(urlCampeones, function(data) {
         var columna = document.createElement("div");
         columna.setAttribute("id", "j"+i);
         columna.className = "col-1 p-0";
+        columna.setAttribute('onclick', 'agregarAlArreglo( "j'+i+'", "divSeleccionados")' );
+        
 
         var img = document.createElement("img");
-        img.src = "http://ddragon.leagueoflegends.com/cdn/12.12.1/img/champion/" + array[i].id + ".png";
+        img.src = "http://ddragon.leagueoflegends.com/cdn/"+ version +"/img/champion/" + array[i].id + ".png";
         img.className = "img-fluid zoom p-0 m-0 "
 
         columna.append(img);
